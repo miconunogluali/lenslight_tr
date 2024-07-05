9dc000a1ecf958ffea83e6f40d3cf874500d9a9f
Ali Miçooğulları <miconunogluali@gmail.com>
2024-07-05 16:06:57 +0300

    big update

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import conn from './db.js';
+
+dotenv.config();
+
+//Connection to the DB
+conn();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+
+// ejs templete engine
+app.set('view engine', 'ejs');
+
+// Static Files middleware
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Index Sayfası ');
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 app.listen(port, () => console.log(`Application running on port : ${port}`));
